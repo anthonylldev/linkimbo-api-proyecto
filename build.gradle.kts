6dbc82033280a7501e152ebfc030b2b4057d581f
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-05-25 21:55:59 +0200

    Implement kmongo dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val koin_version: String by project
+val kmongo_version: String by project
 val logback_version: String by project
 
 plugins {
@@ -37,4 +38,8 @@ dependencies {
     implementation("io.insert-koin:koin-core:$koin_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    // KMongo
+    implementation("org.litote.kmongo:kmongo:$kmongo_version")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
 }
