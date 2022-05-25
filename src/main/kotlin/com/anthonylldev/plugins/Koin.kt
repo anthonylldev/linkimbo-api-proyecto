73e166b920b1c0fde5cf376bb7d72bb21c7d7674
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-05-25 22:06:38 +0200

    Configure Kmongo client

--- src/main/kotlin/com/anthonylldev/plugins/Koin.kt
+++ src/main/kotlin/com/anthonylldev/plugins/Koin.kt
@@ -1,12 +1,20 @@
 package com.anthonylldev.plugins
 
+import com.anthonylldev.util.Constants
 import io.ktor.application.*
 import org.koin.dsl.module
 import org.koin.ktor.ext.Koin
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun Application.configureKoin() {
     install(Koin) {
         modules(module {
+
+            single {
+                val client = KMongo.createClient().coroutine
+                client.getDatabase(Constants.DATABASE_NAME)
+            }
 
         })
     }
---
+++ src/main/kotlin/com/anthonylldev/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.anthonylldev.util
+
+object Constants {
+    const val DATABASE_NAME = "linkimbo"
+}
