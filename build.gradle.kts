820bb09477af4e19242358cfab34e141677530b4
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-06-18 13:23:58 +0200

    Implement fat jar dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.anthonylldev"
